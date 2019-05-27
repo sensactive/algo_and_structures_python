62d92d57327d132250499544646dfc903eb083d2
sensactive <sensor14@list.ru>
2019-05-27 11:07:36 +0300

    lesson4 done

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,77 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+import cProfile
+import timeit
+from random import randint
+
+a = [randint(1, 100) for i in range(100)]
+
+
+# 1.
+
+
+def inc_funcs():
+    max_cnt_value = int()
+    for i in a:
+        if a.count(i) > a.count(max_cnt_value):     # вариант со встроенной функцией
+            max_cnt_value = i
+    # print(max_cnt_value)
+
+
+# 2.
+
+
+def count_with_set():
+    c = 0
+    max_cnt = int()
+    max_cnt_idx = int()
+    cnt = int()
+    b = set(a)
+    for i in b:
+        for c in a:                     # вариант с использованием множества
+            if i == c:
+                cnt += 1
+        if cnt > max_cnt:
+            max_cnt = cnt
+            max_cnt_idx = a.index(i)
+        cnt = 0
+        # print(2, end=' ')
+    # print(a[max_cnt_idx], end=' ')
+
+
+# 3.
+
+
+def count_max():
+    cnt = 0
+    max_cnt = int()
+    max_cnt_idx = int()
+    for i in a:
+        for c in a:                    # вариант без использования множества
+            if i == c:
+                cnt += 1
+        if cnt > max_cnt:
+            max_cnt = cnt
+            max_cnt_idx = a.index(i)
+        cnt = 0
+        # print(1, end=' ')
+    # print(a[max_cnt_idx], end=' ')
+
+
+print(timeit.timeit("inc_funcs()", setup="from __main__ import inc_funcs", number=5))
+print(timeit.timeit("count_with_set()", setup="from __main__ import count_with_set", number=5))
+print(timeit.timeit("count_max()", setup="from __main__ import count_max", number=5))
+
+
+'''
+Сделал замеры трех разных алгоритмов поиска наиболее часто встречающегося числа в массиве.
+Результат меня удивил. Алгоритм №2 с использованием множества, написанный мной работает быстрее,
+чем алгоритм с использованием встроенной ф-ции №1 в 1.5 раза и в 2 раза быстрее, чем алгоритм №3.
+Здесь О(n) - линейная сложность.
+
+Замеры:
+0.004881687000306556 №1
+0.003929732999495172 №2
+0.006941473000551923 №3
+'''
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,74 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+import timeit
+
+
+def sie(lim, n):
+    def sieve(n):
+        a = [x for x in range(n + 1)]
+        a[1] = 0
+        lst = []
+        i = 2
+        while i <= n:
+            if a[i] != 0:
+                lst.append(a[i])
+                for j in range(i, n + 1, i):
+                    a[j] = 0
+            i += 1
+        return lst
+    while len(sieve(n)) < lim:
+        n += 1
+    return sieve(n).pop()
+
+
+def nai(lim, n):
+    def naive_algo(n):
+        # lst = [2]
+        # for i in range(3, n + 1, 2):
+        #     if (i > 10) and (i % 10 == 5):
+        #         continue
+        #     for j in lst:
+        #         if j * j - 1 > i:
+        #             lst.append(i)
+        #             break
+        #         if i % j == 0:
+        #             break
+        #     else:
+        #         lst.append(i)
+        # return lst
+        lst = []
+        for i in range(2, n + 1):
+            for j in range(2, i):
+                if i % j == 0:
+                    # если делитель найден, число не простое.
+                    break
+            else:
+                lst.append(i)
+        return lst
+    while len(naive_algo(n)) < lim:
+        n += 1
+    return naive_algo(n).pop()
+
+
+# lim = int(input("Введите номер простого числа: "))
+# n = 2
+#
+#
+# print(sie(lim, n))
+# print(nai(lim, n))
+
+print(timeit.timeit("sie(100, 2)", setup="from __main__ import sie", number=5))
+print(timeit.timeit("nai(100, 2)", setup="from __main__ import nai", number=5))
+
+
+'''
+Попробовал измерить 3 варианта. Решето работает быстрее на большом порядковом номере простого числа.
+Другие 2 алгоритма работают быстрее на маленьком порядковом номере простого числа(<5).
+
+Замеры при lim = 100:
+0.5556905659996119 - решето
+4.0153251709998585 - наивный алгоритм
+
+Со сложностью - затрудняюсь ответить, но могу предположить, что О(N log N) - линейно-логарифмическая
+'''
