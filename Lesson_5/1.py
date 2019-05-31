7cd59533c806b311c592aa06be9e05c55d565b55
sensactive <sensor14@list.ru>
2019-05-31 20:16:07 +0300

    done

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,73 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import deque, namedtuple
+
+# вариант 1
+
+companies = {}
+number = int(input('Введите количество предприятий: '))
+
+for i in range(number):
+    name = input('Введите наименование предприятия')
+    companies[name] = []
+
+    for q in range(4):
+        profit = int(input('Введите прибыль предприятия за квартал: '))
+        companies[name].append(profit)
+
+counting = {}
+average = 0
+for i in companies:
+    profit = 0
+    for p in companies[i]:
+        profit += p
+    counting[i] = profit
+    average += profit
+
+average = average / number
+
+below = deque()
+above = deque()
+
+for i in counting:
+    if counting[i] < average:
+        below.append(i)
+    else:
+        above.append(i)
+
+print(f'Предприятия с прибылью выше среднего {above} предприятия с прибылью ниже среднего {below}')
+
+# вариант 2
+
+firms = {}
+num = int(input('Введите количество предприятий: '))
+
+firm = namedtuple('firm', 'name first_q second_q third_q forth_q')
+for i in range(num):
+    firms[i] = firm(
+        name=input('Введите наименование предприятия'),
+        first_q=int(input('Введите прибыль предприятия за первый квартал: ')),
+        second_q=int(input('Введите прибыль предприятия за второй квартал: ')),
+        third_q=int(input('Введите прибыль предприятия за третий квартал: ')),
+        forth_q=int(input('Введите прибыль предприятия за четвертый квартал: '))
+    )
+
+firms_tot = {}
+ave = 0
+for i in range(num):
+    firms_tot[firms[i].name] = firms[i].first_q + firms[i].second_q + firms[i].third_q + firms[i].forth_q
+    ave += firms[i].first_q + firms[i].second_q + firms[i].third_q + firms[i].forth_q
+ave = ave / num
+
+bel = deque()
+abo = deque()
+
+for i in firms_tot:
+    if firms_tot[i] < ave:
+        bel.append(i)
+    else:
+        abo.append(i)
+
+print(f'Предприятия с прибылью выше среднего {abo} предприятия с прибылью ниже среднего {bel}')
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,40 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+from collections import deque
+
+mas = input('Введите первое шестнадцатиричное число: ')
+mbs = input('Введите второе шестнадцатиричное число: ')
+
+ma = []
+mb = []
+
+for s in mas:
+    ma.append(s)
+
+for s in mbs:
+    mb.append(s)
+
+a = '0x'
+b = '0x'
+
+for i in ma:
+    a += i
+
+for i in mb:
+    b += i
+
+a, b = int(a, 16), int(b, 16)
+c, d = hex(a + b), hex(a * b)
+
+summ = deque()
+mult = deque()
+
+for i in range(2, len(c)):
+    summ.append(c[i].upper())
+
+for i in range(2, len(d)):
+    mult.append(d[i].upper())
+
+print(f'Для чисел {ma} и {mb} сумма равна {summ}, произведение равно{mult}')
