566d2a718785646ae731b8998f94122f1db40554
sensactive <sensor14@list.ru>
2019-06-04 10:17:03 +0300

    lesson6 done

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -7,3 +7,97 @@
 комментариев к коду. Также укажите в комментариях версию Python
 и разрядность вашей ОС.
 """
+from memory_profiler import profile
+
+
+# @profile
+# def sie(lim, n):
+#     def sieve(n):
+#         a = [x for x in range(n + 1)]
+#         a[1] = 0
+#         lst = []
+#         i = 2
+#         while i <= n:
+#             if a[i] != 0:
+#                 lst.append(a[i])
+#                 for j in range(i, n + 1, i):
+#                     a[j] = 0
+#             i += 1
+#         return lst
+#     while len(sieve(n)) < lim:
+#         n += 1
+#     return sieve(n).pop()
+#
+#
+# @profile
+# def nai(lim, n):
+#     def naive_algo(n):
+#         # lst = [2]
+#         # for i in range(3, n + 1, 2):
+#         #     if (i > 10) and (i % 10 == 5):
+#         #         continue
+#         #     for j in lst:
+#         #         if j * j - 1 > i:
+#         #             lst.append(i)
+#         #             break
+#         #         if i % j == 0:
+#         #             break
+#         #     else:
+#         #         lst.append(i)
+#         # return lst
+#         lst = []
+#         for i in range(2, n + 1):
+#             for j in range(2, i):
+#                 if i % j == 0:
+#                     # если делитель найден, число не простое.
+#                     break
+#             else:
+#                 lst.append(i)
+#         return lst
+#     while len(naive_algo(n)) < lim:
+#         n += 1
+#     return naive_algo(n).pop()
+#
+#
+# lim = 20
+# n = 2
+#
+#
+# print(sie(lim, n))
+# print(nai(lim, n))
+
+@profile
+def function1():
+    a = [i for i in range(10000)]
+    # 1.
+    max_value = int()
+    min_value = a[0]
+    for i in a:
+        if i > max_value:
+            max_value = i
+        elif i < min_value:
+            min_value = i
+    a[a.index(max_value)], a[a.index(min_value)] = a[a.index(min_value)], a[a.index(max_value)]
+
+
+@profile
+def function2():
+    a = [i for i in range(10000)]
+    # 2.
+    a[a.index(max(a))], a[a.index(min(a))] = a[a.index(min(a))], a[a.index(max(a))]
+    a = None
+
+
+if __name__ == "__main__":
+    function1()
+    function2()
+'''
+Python 3.6.7
+x64 (Ubuntu)
+На моем коде решета Эратосфена замеры неинформативные((( Предположу, что это из-за рекурсии.
+Пробовал подставлять разные значения. Всегда вначале выделяется 14.4 MiB и всё(А может быть у меня такой
+алгоритм, что расходуется мало памяти?). Далее взял другой код и инкремент стал появляться при создании 
+списка с числами от 0 до 10000 (+0.2 MiB). После a = None -> -0.2.
+ 
+
+'''
--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,36 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+from pympler.asizeof import asizeof
+
+
+class MyClass1:
+
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+
+
+class MyClass2:
+
+    __slots__ = ['name', 'last_name']
+
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+
+
+a = MyClass1('имя', 'фамилия')
+b = MyClass2('имя', 'фамилия')
+
+print(a.__dict__)
+print(b.__slots__)
+print(asizeof(a))
+print(asizeof(b))
+print(asizeof(a)/asizeof(b))
+
+'''
+Для класса с применением слотов в моём случае памяти выделяется в 2.0357142857142856 меньше, чем 
+для класса без слотов. Но в классе со слотами нет возможности создать атрибуты динамически, например:
+b.address = 'адрес' выдаст ошибку.
+'''
