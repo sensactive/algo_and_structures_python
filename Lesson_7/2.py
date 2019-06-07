27258eaee557b45284b0bdc1e148078f912c76bd
sensactive <sensor14@list.ru>
2019-06-07 14:02:00 +0300

    1-t and 2-d done

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,34 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+from random import randint
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+def mergesort(list):
+    if len(list) < 2:
+        return list
+    middle = len(list) // 2
+    left = mergesort(list[:middle])
+    right = mergesort(list[middle:])
+    return merge(left, right)
+
+
+lst = [randint(0, 50) for _ in range(0, 15)]
+
+print(lst)
+print(mergesort(lst))
