962e1e8d8de89906826e9a07213705ab591df6f2
sensactive <sensor14@list.ru>
2019-06-10 11:20:07 +0300

    2-d done

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,35 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import sys
+
+
+# s = input('введите строку из маленьких латинских букв: ')
+# t = set()
+#
+# for i in range(len(s)):
+#     for j in range(len(s) + 1):
+#         if s[i:j] and s[i:j] != s:
+#             t.add(s[i:j])
+#
+# print(sys.getsizeof(t))
+# print(len(t))
+#
+import hashlib
+
+s = input('введите строку из маленьких латинских букв: ')
+t = set()
+
+for i in range(len(s)):
+    for j in range(len(s) + 1):
+        if s[i:j] and s[i:j] != s:
+            t.add(hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
+
+print(sys.getsizeof(t))
+print(len(t))
+
+
+
+'''
+Абсолютно никакой разницы в размерах я не смог увидеть в этих двух подходах...(( 
+'''
